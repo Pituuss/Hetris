59d507b58ecb81860df8c2ed6966c99662f39b0e
Piotr Wojtyś <pituuuss@gmail.com>
2018-01-07 18:20:13 +0100

    Repair .cabal and .yaml

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,26 @@ module Main where
 
 import Graphics.Gloss
 
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
 
 window :: Display
-window = InWindow "NiceFucker" (200, 200) (10, 10)
+window = InWindow "NiceFucker" (width, height) (offset, offset)
 
 background ::Color
-background  = white
-
-drawing :: Picture
-drawing = circle 80
+background  = black
 
 main :: IO ()
 main = display window background drawing
+
+drawing :: Picture
+drawing = pictures
+  [
+  color ballColor $ circleSolid 20
+  ,color paddleColor $ rectangleSolid 10 50
+  ]
+  where
+    ballColor = dark red
+    paddleColor = light (light blue)
